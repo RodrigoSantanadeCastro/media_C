bdb4626e2c6d108528c92f5b354af55139985f4d
RodrigoSantanadeCastro <rodrigo.santan58@etec.sp.gov.br>
2023-10-16 17:12:14 -0300

    resoluçao

--- main.c
+++ main.c
@@ -5,19 +5,24 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	float res, valor;
+	float res, valor,diferenca;
 	setlocale (LC_ALL,"Portuguese");
-	printf("Digite o valor do produto\n");
+	do{
+	printf("\nCalculadora de inflação\n");
+	printf("Digite o valor do produto\nou 0 para sair\n");
 	scanf("%f", &valor);
 	if(valor<=99){
 		res=valor/100*10+valor;
-		printf("O preço era %f e passou a ser %f",valor,res);
-		return 0;
+		diferenca=valor/100*10;
+		printf("O preço era de %f e passou a ser %f, o valor sofreu reajuste de %f\n",valor,res,diferenca);
 	}
 	if(valor>=100){
 		res=valor/100*20+valor;
-		printf("O preço era %f e passou a ser %f",valor,res);
-		return 0;
-	}
-	return 0;
+		diferenca=valor/100*20;
+		printf("O preço de %f e passou a ser %f,o valor sofreu reajuste de %f\n",valor,res,diferenca);
+	} 
+} while(valor>0);
+return 0;
 }
+
+
