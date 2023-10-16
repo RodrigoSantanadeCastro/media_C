632c27b41e7dc3828079cbad8cbd6149ba244953
RodrigoSantanadeCastro <rodrigo.santan58@etec.sp.gov.br>
2023-10-16 17:13:45 -0300

    last

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
 	if(valor>=100){
 		res=valor/100*20+valor;
 		diferenca=valor/100*20;
-		printf("O preço de %f e passou a ser %f,o valor sofreu reajuste de %f\n",valor,res,diferenca);
+		printf("O preço era de %f e passou a ser %f,o valor sofreu reajuste de %f\n",valor,res,diferenca);
 	} 
 } while(valor>0);
 return 0;
